4dfbc2427a0fba9265ccbaeb0060664e065d4777
creakosta <creakosta@mail.ru>
2024-01-17 12:21:41 +0300

    Update DeploymentTargetFix.js

--- hooks/DeploymentTargetFix.js
+++ hooks/DeploymentTargetFix.js
@@ -50,50 +50,6 @@ post_install do |installer|
       config.build_settings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "arm64"
     end
   end
-  
-  
-  
-  installer.aggregate_targets.each do |target|
-    target.xcconfigs.each do |variant, xcconfig|
-      xcconfig_path = target.client_root + target.xcconfig_relative_path(variant)
-      IO.write(xcconfig_path, IO.read(xcconfig_path).gsub("DT_TOOLCHAIN_DIR", "TOOLCHAIN_DIR"))
-    end
-  end
-  installer.pods_project.targets.each do |target|
-    target.build_configurations.each do |config|
-      if config.base_configuration_reference.is_a? Xcodeproj::Project::Object::PBXFileReference
-        xcconfig_path = config.base_configuration_reference.real_path
-        IO.write(xcconfig_path, IO.read(xcconfig_path).gsub("DT_TOOLCHAIN_DIR", "TOOLCHAIN_DIR"))
-      end
-    end
-  end
-
-    installer.aggregate_targets.each do |aggregate_target|
-    if aggregate_target.name == 'Pods-[MY_APP_TARGET]'
-      aggregate_target.xcconfigs.each do |config_name, config_file|
-        sharedLibraryPodTargets = sharedLibrary.pod_targets
-        aggregate_target.pod_targets.select { |pod_target| sharedLibraryPodTargets.include?(pod_target) }.each do |pod_target|
-          pod_target.specs.each do |spec|
-            frameworkPaths = unless spec.attributes_hash['ios'].nil? then spec.attributes_hash['ios']['vendored_frameworks'] else spec.attributes_hash['vendored_frameworks'] end || Set.new
-            frameworkNames = Array(frameworkPaths).map(&:to_s).map do |filename|
-              extension = File.extname filename
-              File.basename filename, extension
-            end
-          end
-          frameworkNames.each do |name|
-            if name != '[DUPLICATED_FRAMEWORK_1]' && name != '[DUPLICATED_FRAMEWORK_2]'
-              raise("Script is trying to remove unwanted flags: #{name}. Check it out!")
-            end
-            puts "Removing #{name} from OTHER_LDFLAGS"
-            config_file.frameworks.delete(name)
-          end
-        end
-      end
-      xcconfig_path = aggregate_target.xcconfig_path(config_name)
-      config_file.save_as(xcconfig_path)
-    end
-  end
-end
 
   `;
 

94dd7ab5215222720433b682289e0f08dc5b9d24
creakosta <creakosta@mail.ru>
2024-01-17 11:33:36 +0300

    Update DeploymentTargetFix.js

--- hooks/DeploymentTargetFix.js
+++ hooks/DeploymentTargetFix.js
@@ -1,45 +1,46 @@
-#!/usr/bin/env node
-
-// Workaround for https://github.com/dpa99c/cordova-plugin-firebasex/issues/766
-// set CODE_SIGNING_ALLOWED to NO to avoid signing errors during CI Builds
-
-const fs = require("fs");
-const path = require("path");
-const execa = require("execa");
-
-module.exports = (context) => {
-  const platformPath = path.resolve(context.opts.projectRoot, "platforms/ios");
-  const podfilePath = path.resolve(platformPath, "Podfile");
-
-  if (!fs.existsSync(podfilePath)) {
-    console.log(`'${podfilePath}' does not exist. Firebase deployment fix skipped.`);
-    return;
-  }
-
-  let podfileContent = fs.readFileSync(podfilePath, "utf-8");
-  if (podfileContent.indexOf("post_install") == -1) {
-    podfileContent += `
+pod_targets_for_disable_build_for_distribution = [
+     'DivKit',
+     'DivKit_Base',
+     'DivKit_BaseTiny',
+     'DivKit_BaseUI',
+     'DivKit_LayoutKit',
+     'DivKit_LayoutKitInterface',
+     'DivKit_CommonCore',
+     'DivKit_Serialization',
+     'DivKit_Networking'
+]
 
 post_install do |installer|
   installer.pods_project.targets.each do |target|
+           if target.name.start_with?(*pod_targets_for_disable_build_for_distribution)
+              target.build_configurations.each do |config|
+                  config.build_settings['BUILD_LIBRARY_FOR_DISTRIBUTION'] = 'NO'
+              end
+          end
+      end
+  
+  sharedLibrary = installer.aggregate_targets.find { |aggregate_target| aggregate_target.name == 'Pods-[MY_FRAMEWORK_TARGET]' }
+  
+ 
+  installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
-      config.build_settings.delete 'IPHONEOS_DEPLOYMENT_TARGET'
-      config.build_settings['ONLYARCHIVEARCH'] = 'YES'
-      config.build_settings['ONLY_ACTIVE_ARCH'] = 'YES'
-      config.build_settings['CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER'] = 'NO'
-      config.build_settings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "arm64 i386"
-      config.build_settings["EXCLUDED_ARCHS[sdk=iphoneos*]"] = "x86_64"
       config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
     end
   end
-end
-
-`;
-
-    fs.writeFileSync(podfilePath, podfileContent, "utf-8");
-
-    return execa("pod", ["install", "--verbose"], {
-      cwd: platformPath,
-    });
-  }
-};
+  
+  
+  
+  installer.aggregate_targets.each do |target|
+    target.xcconfigs.each do |variant, xcconfig|
+      xcconfig_path = target.client_root + target.xcconfig_relative_path(variant)
+      IO.write(xcconfig_path, IO.read(xcconfig_path).gsub("DT_TOOLCHAIN_DIR", "TOOLCHAIN_DIR"))
+    end
+  end
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      if config.base_configuration_reference.is_a? Xcodeproj::Project::Object::PBXFileReference
+        xcconfig_path = config.base_configuration_reference.real_path
+        IO.write(xcconfig_path, IO.read(xcconfig_path).gsub("DT_TOOLCHAIN_DIR", "TOOLCHAIN_DIR"))
+      end
+    end
+  end

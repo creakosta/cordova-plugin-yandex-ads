83046240d6796a4e49af92d3210ee7a3e29399c9
creakosta <creakosta@mail.ru>
2024-01-17 16:00:03 +0300

    Update DeploymentTargetFix.js

--- hooks/DeploymentTargetFix.js
+++ hooks/DeploymentTargetFix.js
@@ -43,16 +43,9 @@ post_install do |installer|
 
   installer.pods_project.targets.each do |target|
     target.build_configurations.each do |config|
-      config.build_settings.delete 'IPHONEOS_DEPLOYMENT_TARGET'
-      config.build_settings['ONLYARCHIVEARCH'] = 'YES'
-      config.build_settings['ONLY_ACTIVE_ARCH'] = 'YES'
-      config.build_settings['CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER'] = 'NO'
-      config.build_settings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "arm64"
-      config.build_settings["EXCLUDED_ARCHS[sdk=iphoneos*]"] = "armv7 armv7s"
       config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
     end
   end
-end
 
   `;
 

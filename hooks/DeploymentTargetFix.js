51135ad37a0dde9cb6f76b1299663f1dc97ed90d
creakosta <creakosta@mail.ru>
2024-01-19 15:18:26 +0300

    Update DeploymentTargetFix.js

--- hooks/DeploymentTargetFix.js
+++ hooks/DeploymentTargetFix.js
@@ -22,7 +22,7 @@ post_install do |installer|
     project.targets.each do |target|
       target.build_configurations.each do |config|
         config.build_settings['ONLY_ACTIVE_ARCH'] = 'NO'
-        config.build_settings['SWIFT_VERSION'] = '5.8'
+        config.build_settings['EXCLUDED_ARCHS[sdk=iphonesimulator*]'] = 'arm64'
         config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
         xcconfig_path = config.base_configuration_reference.real_path
         xcconfig = File.read(xcconfig_path)

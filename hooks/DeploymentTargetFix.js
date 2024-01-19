088e43652538c6c36d7f857492c08239bebc4e2c
creakosta <creakosta@mail.ru>
2024-01-19 16:54:39 +0300

    Update DeploymentTargetFix.js

--- hooks/DeploymentTargetFix.js
+++ hooks/DeploymentTargetFix.js
@@ -45,7 +45,6 @@ post_install do |installer|
   installer.generated_projects.each do |project|
     project.targets.each do |target|
       target.build_configurations.each do |config|
-        config.build_settings['ONLY_ACTIVE_ARCH'] = 'NO'
         config.build_settings['BUILD_LIBRARY_FOR_DISTRIBUTION'] = 'NO'
         config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
         xcconfig_path = config.base_configuration_reference.real_path

e8739a1e7b9bcf444df8ca1967da217c1be8a93a
creakosta <creakosta@mail.ru>
2024-01-19 13:11:57 +0300

    Update DeploymentTargetFix.js

--- hooks/DeploymentTargetFix.js
+++ hooks/DeploymentTargetFix.js
@@ -22,6 +22,7 @@ post_install do |installer|
     project.targets.each do |target|
       target.build_configurations.each do |config|
         config.build_settings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "arm64"
+        config.build_settings['ENABLE_BITCODE'] = 'NO'
         config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
         xcconfig_path = config.base_configuration_reference.real_path
         xcconfig = File.read(xcconfig_path)

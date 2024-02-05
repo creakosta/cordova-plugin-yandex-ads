ede098efa978bd2504858236c4378709d7bad5ee
creakosta <creakosta@mail.ru>
2024-02-05 11:42:24 +0300

    Add files via upload

--- src/ios/YandexAdsPlugin.swift
+++ src/ios/YandexAdsPlugin.swift
@@ -9,10 +9,10 @@ let PLUGIN_NOT_INITIALIZED_ERROR = [
 
 @objc(YandexAdsPlugin)
 class YandexAdsPlugin: CDVPlugin {
-    private var rewardedBlockId: String?
-    private var interstitialBlockId: String?
-    private var bannerBlockId: String?
-    private var openAppBlockId: String?
+    public var rewardedBlockId: String?
+    public var interstitialBlockId: String?
+    public var bannerBlockId: String?
+    public var openAppBlockId: String?
 
     public var bannerAtTop: Bool?
     public var bannerSize: NSDictionary?
@@ -20,9 +20,22 @@ class YandexAdsPlugin: CDVPlugin {
     public var interstitialAd: YMAInterstitialAd?
     public var rewardedAd: YMARewardedAd?
     public var appOpenAd: YMAAppOpenAd?
+    public var bannerAdViewCache: YMAAdView?
+
+    public var superView: UIView?
+    public var stackViewInlineBannerView: UIStackView?
+    public var bannerStackView: UIView?
 
     override init() {
+        super.init()
+
         self.bannerAtTop = false
+    }
+
+    override func pluginInitialize() {
+        super.pluginInitialize()
+
+        self.superView = webView.superview
     }
 
     @objc(run:)
@@ -41,75 +54,7 @@ class YandexAdsPlugin: CDVPlugin {
         self.sendResult(command: command);
     }
 
-    private var bannerAdViewCache: YMAAdView?;
-
-    func getBannerAdView() -> YMAAdView {
-        if self.bannerAdViewCache != nil {
-            return self.bannerAdViewCache!
-        }
-
-        let width = self.webView.safeAreaLayoutGuide.layoutFrame.width
-        var adSize = YMABannerAdSize.stickySize(withContainerWidth: width)
-
-        if (self.bannerSize != nil && self.bannerSize?["width"] != nil && self.bannerSize?["height"] != nil) {
-            adSize = YMABannerAdSize.inlineSize(withWidth: self.bannerSize?["width"] as! CGFloat, maxHeight: self.bannerSize?["height"] as! CGFloat)
-        }
-
-        let adView = YMAAdView(adUnitID: self.bannerBlockId!, adSize: adSize)
-
-        adView.delegate = self
-
-        self.bannerAdViewCache = adView
-
-        return adView
-    }
-
-    @objc(loadBanner:)
-    func loadBanner(command: CDVInvokedUrlCommand) {
-        if self.bannerBlockId == nil {
-            self.sendError(command: command, code: PLUGIN_NOT_INITIALIZED_ERROR["code"]!, message: PLUGIN_NOT_INITIALIZED_ERROR["message"]!);
-            return;
-        }
-
-        self.getBannerAdView().loadAd()
-
-        self.sendResult(command: command);
-    }
-
-    @objc(showBanner:)
-    func showBanner(command: CDVInvokedUrlCommand) {
-        if self.bannerBlockId == nil {
-            self.sendError(command: command, code: PLUGIN_NOT_INITIALIZED_ERROR["code"]!, message: PLUGIN_NOT_INITIALIZED_ERROR["message"]!);
-            return;
-        }
-
-        let banner = self.getBannerAdView();
-        print(banner)
-
-        if self.bannerAtTop != nil && self.bannerAtTop == true {
-            banner.displayAtTop(in: webView)
-        } else {
-            banner.displayAtBottom(in: webView)
-        }
-
-        self.sendResult(command: command);
-    }
-
-    @objc(hideBanner:)
-    func hideBanner(command: CDVInvokedUrlCommand) {
-        if self.bannerBlockId == nil {
-            self.sendError(command: command, code: PLUGIN_NOT_INITIALIZED_ERROR["code"]!, message: PLUGIN_NOT_INITIALIZED_ERROR["message"]!);
-            return;
-        }
-
-        self.getBannerAdView().delegate = nil
-        self.getBannerAdView().removeFromSuperview()
-        self.bannerAdViewCache = nil
-
-        self.sendResult(command: command);
-    }
-
-    private lazy var interstitialAdLoader: YMAInterstitialAdLoader = {
+    public lazy var interstitialAdLoader: YMAInterstitialAdLoader = {
         let loader = YMAInterstitialAdLoader()
 
         loader.delegate = self
@@ -117,32 +62,7 @@ class YandexAdsPlugin: CDVPlugin {
         return loader
     }()
 
-    @objc(loadInterstitial:)
-    func loadInterstitial(command: CDVInvokedUrlCommand) {
-        if self.interstitialBlockId == nil {
-            self.sendError(command: command, code: PLUGIN_NOT_INITIALIZED_ERROR["code"]!, message: PLUGIN_NOT_INITIALIZED_ERROR["message"]!);
-            return;
-        }
-
-        let configuration = YMAAdRequestConfiguration(adUnitID: self.interstitialBlockId!)
-        self.interstitialAdLoader.loadAd(with: configuration)
-
-        self.sendResult(command: command);
-    }
-
-    @objc(showInterstitial:)
-    func showInterstitial(command: CDVInvokedUrlCommand) {
-        if self.interstitialBlockId == nil {
-            self.sendError(command: command, code: PLUGIN_NOT_INITIALIZED_ERROR["code"]!, message: PLUGIN_NOT_INITIALIZED_ERROR["message"]!);
-            return;
-        }
-
-        self.interstitialAd?.show(from: viewController)
-
-        self.sendResult(command: command);
-    }
-
-    private lazy var rewardedAdLoader: YMARewardedAdLoader = {
+    public lazy var rewardedAdLoader: YMARewardedAdLoader = {
         let loader = YMARewardedAdLoader()
 
         loader.delegate = self
@@ -150,61 +70,13 @@ class YandexAdsPlugin: CDVPlugin {
         return loader
     }()
 
-    @objc(loadRewardedVideo:)
-    func loadRewardedVideo(command: CDVInvokedUrlCommand) {
-        if self.rewardedBlockId == nil {
-            self.sendError(command: command, code: PLUGIN_NOT_INITIALIZED_ERROR["code"]!, message: PLUGIN_NOT_INITIALIZED_ERROR["message"]!);
-            return;
-        }
+    public lazy var appOpenAdLoader: YMAAppOpenAdLoader = {
+        let loader = YMAAppOpenAdLoader()
 
-        let configuration = YMAAdRequestConfiguration(adUnitID: self.rewardedBlockId!)
-        self.rewardedAdLoader.loadAd(with: configuration)
+        loader.delegate = self
 
-        self.sendResult(command: command);
-    }
-
-    @objc(showRewardedVideo:)
-    func showRewardedVideo(command: CDVInvokedUrlCommand) {
-        if self.rewardedBlockId == nil {
-            self.sendError(command: command, code: PLUGIN_NOT_INITIALIZED_ERROR["code"]!, message: PLUGIN_NOT_INITIALIZED_ERROR["message"]!);
-            return;
-        }
-
-        self.rewardedAd?.show(from: viewController)
-
-        self.sendResult(command: command);
-    }
-
-    private lazy var appOpenAdLoader: YMAAppOpenAdLoader = {
-            let loader = YMAAppOpenAdLoader()
-            loader.delegate = self
-            return loader
+        return loader
     }()
-
-    @objc(loadOpenAppAds:)
-    func loadOpenAppAds(command: CDVInvokedUrlCommand) {
-        if self.openAppBlockId == nil {
-            self.sendError(command: command, code: PLUGIN_NOT_INITIALIZED_ERROR["code"]!, message: PLUGIN_NOT_INITIALIZED_ERROR["message"]!);
-            return;
-        }
-
-        let configuration = YMAAdRequestConfiguration(adUnitID: self.openAppBlockId!)
-        appOpenAdLoader.loadAd(with: configuration)
-
-        self.sendResult(command: command);
-    }
-
-    @objc(showOpenAppAds:)
-    func showOpenAppAds(command: CDVInvokedUrlCommand) {
-        if self.openAppBlockId == nil {
-            self.sendError(command: command, code: PLUGIN_NOT_INITIALIZED_ERROR["code"]!, message: PLUGIN_NOT_INITIALIZED_ERROR["message"]!);
-            return;
-        }
-
-        self.appOpenAd?.show(from: viewController)
-
-        self.sendResult(command: command);
-    }
 
     @objc(setUserConsent:)
     func setUserConsent(command: CDVInvokedUrlCommand) {
